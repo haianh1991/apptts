c775a55a7e82ed341c7dcd2987d2b868066394f7
haianh1991 <haianh17119@gmail.com>
2026-05-24 16:11:35 +0700

    Bump version to v1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.webreader"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.4.0"
+        versionCode = 15
+        versionName = "1.4.1"
     }
 
     buildTypes {
