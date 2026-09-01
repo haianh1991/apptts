6f877c3f1fd66f28e48817ba335d366bdae0b6b4
haianh1991 <haianh17119@gmail.com>
2026-09-01 09:44:54 +0700

    Fix release APK signing config with debug certificate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
         release {
             isMinifyEnabled = false
             isCrunchPngs = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
